ee0a531530c2f53cc8a978429833f5d47867399f
Jack Draak <jackdraak@gmail.com>
2017-01-01 17:15:11 -0500

    two forward, one back

--- ZombieMan/ZombieManMVC.cpp
+++ ZombieMan/ZombieManMVC.cpp
@@ -22,11 +22,14 @@
 #include <iostream>
 #include <sstream>
 
-// Include in _order_ to be random!
-#include <random> 
-
 // Include in _order_ to sort!
 #include <algorithm>
+
+// Include in _order_ to map relations!
+#include <map>
+
+// Include in _order_ to be random!
+#include <random> 
 
 // Includes to treat the console display as a textbox.
 #include <windows.h>
@@ -78,6 +81,7 @@ char GetLetter();
 int GetPercent();
 int Shuffle(int, int);
 std::string GetString();
+std::string SetZombieWord(int, int);
 void PauseForInput();
 void PrintLetterBox();
 bool PrintAndEvaluateSecret();
@@ -203,7 +207,7 @@ int GetPercent()
     Home(21, 0); std::cout << "                   ";
     do
     {
-        Home(20, 44);
+        Home(19, 44);
         getline(std::cin, userInput);
         Home(21, 0); std::cout << "                                  ";
         std::stringstream thisStream(userInput);
@@ -258,7 +262,7 @@ void Home(int row, int column)
 void PauseForInput()
 {
     std::string userInput;
-    Home(20, 0);  std::cout << "] Please hit <Enter> to proceed...                       ";
+    Home(19, 0);  std::cout << "] Please hit <Enter> to proceed...                       ";
     getline(std::cin, userInput);
     return;
 }
@@ -271,7 +275,7 @@ bool Continue()
     do
     {
         std::string userInput;
-        Home(20, 0); std::cout << "] Please enter (P)lay or (Q)uit then <Enter>, thank you. ";
+        Home(19, 0); std::cout << "] Please enter (P)lay or (Q)uit then <Enter>, thank you. ";
         getline(std::cin, userInput);
         if (userInput[0] == 'p' || userInput[0] == 'P')
         {
@@ -285,6 +289,145 @@ bool Continue()
     } while (!validInput); 
     Home(10, 0); std::cout << "                                                         ";
     return proceed;
+}
+
+// Respond with # of guesses allowed based on length of challenge word (& difficulty setting?).
+int GetMaxGuesses(int wordSize) 
+{
+    int maxGuesses;
+
+    if (true) /// (difficultyFactor == 1) // Easy difficulty map.
+    {
+        std::map <int, int> mWordSizeToGuessCount{
+            { 2,11 },{ 3,10 },{ 4,9 },{ 5,8 },{ 6,7 },{ 7,6 },{ 8,5 },{ 9,4 },
+            { 10,3 },{ 11,3 },{ 12,3 },{ 13,3 },{ 14,3 },{ 15,3 },{ 16,3 },{ 17,3 }
+        };
+        maxGuesses = mWordSizeToGuessCount[wordSize];
+    } return maxGuesses;
+}
+
+// Respond with next challenge-word;
+std::string SetZombieWord(int minSize, int maxSize)
+{
+    if (maxSize < 3) { maxSize = 3; }
+    if (maxSize > 11) { maxSize = 11; }
+    if (minSize < 2) { minSize = 2; }
+    if (minSize > 10) { minSize = 10; }
+
+    // Dictionary of isogram challenge words
+    std::vector<std::string> dictionary = {
+        // random words, may not be isograms
+        "that", "ranger", "endanger", "entangle", "arrest", "arresting", "arrested", "envelope", "raspberry", "cherry", 
+        "gross", "cheese", "homesteading", "homestead", "pioneer", "pioneering", "evaluate", "evaluating", "evaluated",
+        "risks", "mitigation", "enshrine", "enshrined", "allowed", "maulers", "brawlers", "rookies", "provision", "revision",
+        "envision", "envisioning", "envisioned", "challenge", "arrange", "arranged", "alabaster", "inevitable", "glorious",
+        "endangered", "whistle", "criminal", "justice", "system", "moment", "before", "momentary", "realize", "realizing",
+
+        // 2
+        "as", "am", "an", "at", "be", "do", "go", "he", "id", "is", "it", "me", "no", "on", "so", "to", "we",
+
+        // 3
+        "age", "art", "bag", "bat", "boy", "bye", "car", "coy", "cry", "cut", "day", "dim", "din", "elf", "fit",
+        "fly", "for", "hit", "hug", "jam", "kit", "lab", "lax", "lie", "lit", "lot", "mar", "may", "mit", "net",
+        "nip", "par", "pay", "pen", "pie", "pit", "pot", "put", "rag", "ray", "rot", "say", "sea", "sin", "tag",
+        "tax", "tea", "the", "try", "vye", "was", "win", "won", "yam", "zip",
+
+        // 4
+        "acre", "ages", "army", "aunt", "bail", "bale", "belt", "care", "cave", "cure", "daft", "damn", "dart",
+        "dime", "expo", "fair", "fast", "fawn", "felt", "fish", "from", "hair", "hare", "harp", "hear", "help",
+        "hide", "huge", "jail", "july", "june", "lair", "land", "lend", "lint", "love", "lynx", "lyre", "name",
+        "nape", "navy", "nose", "oven", "pair", "pure", "quit", "raid", "rail", "same", "sail", "sand", "sock",
+        "soda", "sold", "soup", "suit", "talk", "tiny", "this", "tone", "tray", "tuna", "walk", "wasp", "wave",
+        "yarn",
+
+        // 5
+        "angel", "anger", "angry", "ankle", "april", "blast", "braid", "brain", "bread", "brick", "brush", "clear",
+        "cloth", "drain", "dregs", "dwarf", "empty", "enact", "envoy", "facts", "fairy", "faith", "farce", "farms",
+        "gravy", "hares", "large", "laugh", "light", "lingo", "litre", "lives", "loads", "locks", "march", "marsh",
+        "miles", "money", "nasty", "nurse", "pious", "query", "rages", "roads", "roast", "sable", "scone", "shock",
+        "sight", "snout", "stand", "stick", "super", "swift", "table", "talks", "tango", "thick", "thing", "tiger",
+        "timer", "toads", "tongs", "trash", "trick", "tuber", "tuner", "vegan", "yeast",
+
+        // 6
+        "abound", "around", "baster", "belfry", "bounty", "bricks", "bright", "bronze", "buster", "cables",
+        "crawls", "crowns", "curled", "custom", "dancer", "danger", "dearly", "design", "fables", "fabric",
+        "factor", "fairly", "famine", "farces", "friday", "gizmos", "guitar", "hybrid", "jaunts", "loving",
+        "market", "monday", "muster", "myrtle", "mythos", "nachos", "nailed", "nebula", "nickel", "orgasm",
+        "phrase", "placid", "quaint", "quartz", "sailor", "sample", "sculpt", "second", "slight", "sucker",
+        "sunday", "tackle", "tongue", "travel", "tricks", "turkey", "wealth", "zealot",
+
+        // 7
+        "anxiety", "apricot", "caribou", "cliquey", "clothes", "consume", "crazily", "dolphin", "domains",
+        "exclaim", "fabrics", "factory", "haircut", "imports", "isogram", "jackpot", "jaybird", "jockeys",
+        "jukebox", "lockjaw", "mindful", "monarch", "nostril", "polearm", "pulsing", "quicken", "quicker",
+        "quickly", "scarlet", "schlump", "scholar", "scourge", "sketchy", "smoking", "sparkle", "strange",
+        "stymied", "subject", "treason", "turbine", "wrongly", "wyverns", "yoghurt", "ziplock",
+
+        // 8 --
+        "authored", "bankrupt", "blasting", "bunghole", "burliest", "bushland", "bushmeat", "busywork",
+        "butchery", "buxomest", "cogently", "crumpets", "emblazon", "exhaling", "exoplasm", "explains",
+        "exploits", "finagled", "frontage", "goldfish", "gumption", "handgrip", "hardiest", "hasteful",
+        "hospital", "imported", "informed", "jockular", "megalith", "megatons", "merciful", "minerals",
+        "monarchy", "pronated", "questing", "question", "rambling", "sculptor", "speaking", "spectrum",
+        "thursday",
+
+        // 9
+        "authorize", "binocular", "constable", "dangerous", "dragonfly", "inspector", "introduce", "nightmare",
+        "racheting", "ramblings", "redaction", "revolting", "solarized", "solemnity", "traveling", "tribunals",
+        "uncharted", "unclamped", "veracious", "verbosity", "vouchsafe", "vulcanism", "vulgarity", "watchdogs",
+        "waveforms", "wavefront", "welcoming", "windstorm", "wolfsbane", "womanizer", "wordgames", "workmates",
+        "workplace", "workspace", "worshiped", "yachtsmen",
+
+        // 10
+        "abductions", "afterglows", "aftershock", "agonizedly", "algorithms", "authorizes", "background",
+        "behaviours", "benchmarks", "binoculars", "birthnames", "campground", "capsulized", "chalkstone",
+        "chipboards", "chlorinate", "destroying", "duplicates", "exfoliants", "exhaustion", "farsighted",
+        "gobsmacked", "graciously", "greyhounds", "hospitable", "infamously", "nightmares", "shockingly",
+        "subjectify", "sympathize",
+
+        // 11
+        "abolishment", "atmospheric", "backgrounds", "backlighted", "birthplaces", "bodysurfing", "cabinetwork",
+        "countryside", "dangerously", "decryptions", "delusionary", "demographic", "designatory", "discernably",
+        "documentary", "exculpatory", "factorylike", "flavourings", "francophile", "palindromes", "personality",
+        "playgrounds", "precautions", "regulations", "speculation", "subcategory", "sympathizer", "trampolines",
+        "workmanship",
+
+        // 12
+        "counterplays", "demographics", "demonstrably", "descrambling", "discountable", "discrepantly", "exhaustingly",
+        "lexicography", "locksmithery", "malnourished", "multipronged", "nightwalkers", "outsparkling", "overstudying",
+        "packinghouse", "productively", "pyromagnetic", "questionably", "stenographic", "stickhandler", "subnormality",
+        "subvocalized", "thunderclaps", "unforgivable", "unprofitable", "upholstering",
+
+        // 13
+        "chimneyboards", "consumptively", "copyrightable", "documentarily", "draughtswomen", "flamethrowing",
+        "flowchartings", "lycanthropies", "metalworkings", "misconjugated", "muckspreading", "multibranched",
+        "musicotherapy", "pneumogastric", "salpingectomy", "subordinately", "unmaledictory", "unpredictably",
+        "unproblematic", "unsympathized",
+
+        // 14
+        "ambidextrously", "computerizably", "dermatoglyphic", "hydromagnetics", "hydropneumatic", "pseudomythical",
+        "subformatively", "undiscoverably",
+
+        // 15
+        "uncopyrightable",
+
+        // 17
+        "subdermatoglyphic"
+    };
+    int numberOfWords = size(dictionary);
+
+    // Select the challenge word randomly.
+    std::string selectionString;
+    int randomNumber;
+    int selectionLength;
+
+    do {
+        std::uniform_int_distribution<> zIndexDist(0, numberOfWords - 1);
+        randomNumber = zIndexDist(Entropy);
+        selectionString = dictionary[randomNumber];
+        selectionLength = selectionString.length();
+    } while (selectionLength < minSize || selectionLength > maxSize);
+    return selectionString; // Break and watch this variable to cheat,^H^H^H^H^H^H^H^H^Herr for play-testing.
 }
 
 // ----- Letter-Box Methods ----- //
@@ -338,38 +481,42 @@ int main()
         int zombieLevel = 0;
         bool zombieSafe = true;
         bool success;
+        secretWord = SetZombieWord(3, 11);
+        int guessLimit = GetMaxGuesses(secretWord.size());
+        int zombieBooster = (99.0f / guessLimit);
+
         activeLetterBox.Reset();
         do
         {
             Home();
             PrintLetterBox();
-            PrintZombieMan(zombieLevel); // Shuffle(0, 100)); // TODO placeholder for something more meaningful ||  PrintZombieMan(zombieLevel);
+            PrintZombieMan(zombieLevel); /// Shuffle(0, 100));
             success = PrintAndEvaluateSecret();
             if (zombieLevel >= 100)
             {
                 success = true;
-                Home(20, 42); std::cout << "              ";
-                Home(20, 0); std::cout << "] Here comes ZombieMan, intent on eating your BrAiNs!\n  (hit <Enter> to continue)";
+                Home(19, 42); std::cout << "              ";
+                Home(19, 0); std::cout << "] Here comes ZombieMan, intent on eating your BrAiNs!\n  (hit <Enter> to continue)";
                 std::string thisPlay = GetString();
             }
             else if (!success) {
-                Home(20, 42); std::cout << "              ";
-                Home(20, 0); std::cout << "] Please enter the letter you would like to risk: ";
+                Home(19, 42); std::cout << "              ";
+                Home(19, 0); std::cout << "] Please enter the letter you would like to risk: ";
                 std::string thisPlay = GetString();
                 bool zombieSafe = (activeLetterBox.SubmitLetter(thisPlay[0]));
                 if (!zombieSafe)
                 {
-                    zombieLevel += 20;
+                    zombieLevel += zombieBooster;
                 }
             }
         } while (!success);
         if (PrintAndEvaluateSecret())
         {
-            Home(20, 42); std::cout << "              ";
-            Home(20, 0); std::cout << "] You escaped with the secret word before ZombieMan attacked. Congratulations!\n  (hit <Enter> to continue)";
+            Home(19, 42); std::cout << "              ";
+            Home(19, 0); std::cout << "] You escaped with the secret word before ZombieMan attacked. Congratulations!\n  (hit <Enter> to continue)";
             std::string thisPlay = GetString();
         }
-    } while (true); //  Continue());
+    } while (true); ///  Continue());
 
     return 0;
 }
